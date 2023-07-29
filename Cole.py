a71b461e6eb75e3ec218aff86bfda873cfd60d63
colegiusto <83982339+colegiusto@users.noreply.github.com>
2023-07-29 16:46:13 -0700

    dsd

--- Cole.py
+++ Cole.py
@@ -1,13 +1,16 @@
 from utils import Ship, Board, Player
-from typing import Tuple
+from typing import Tuple, List
 
 
 
 class StatPlayer(Player):
-    def place_ships(sizes: list[int]):
+    
+    
+
+    def place_ships(self, sizes: List[int]) -> List[Ship]:
         pass
 
-    def make_play():
+    def make_play() -> Tuple[int,int]:
         pass
 
     def opponent_play(play: Tuple[int, int]):
