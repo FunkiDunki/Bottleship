85ef8c7f6794c91fa49ba8a43e384ec09295edc4
JordonGiusto <jordon.giusto@gmail.com>
2023-07-29 11:49:14 -0700

    addded print to board utitls

---
+++ utils.py
@@ -0,0 +1,22 @@
+def print_board(board):
+
+    for i in range(board["y"]):
+        buff = chr(64 + board["y"] - i)
+        buff += '   '
+        for j in range(board["x"]):
+            buff += '.' + '  '
+
+        print(buff)
+    print()
+    buff = '   '
+    for i in range(board["x"]):
+        buff += str(i) + '  '
+    print(buff)
+
+
+board = {
+    "x": 10,
+    "y":10,
+}
+
+print_board(board)
