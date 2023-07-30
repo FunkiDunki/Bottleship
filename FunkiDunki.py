4846e0ecb4cabfdf398e1c45722bcbf146f7e124
FunkiDunki <nicholashotelling@gmail.com>
2023-07-29 17:05:48 -0700

    made stuff

---
+++ FunkiDunki.py
@@ -0,0 +1,41 @@
+from utils import *
+
+class FunkiDunki (Player):
+    def __init__(self, size: int):
+        self.oppo = Board(size)
+        self.size = size
+        self.myboard = Board(size)
+        self.ships = []
+        self.t = 0
+
+    def PlaceShips(self, sizes: list[int]):
+        dirs = [[1,0], [0,1]]
+        posits = []
+        for i in range(self.size):
+            for j in range(self.size):
+                posits.append([i,j])
+        for si in sizes:
+            found = False
+            for pos in posits:
+                for dir in dirs:
+                    sh = Ship(si, pos, dir)
+                    li = self.ships
+                    li.append(sh)
+                    if self.myboard.check_ship_list(li):
+                        self.ships.append(sh)
+                        found = True
+                        break
+                if found:
+                    break
+        return(self.ships)
+
+    def MakePlay(self):
+        play = [self.t%self.size, self.t/self.size]
+        self.t+= 1
+        return play
+
+    def OpponentPlay(self, play: tuple[int, int]):
+        pass
+
+    def TurnFeedback(self, play: tuple[int, int], hit: bool, sink: bool):
+        pass
